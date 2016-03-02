993344a58a1054ddee1c34cad3663cda7fb009a8
EvanR <evanrinehart@gmail.com>
2016-03-02 08:37:58 -0600

    check 90% hack

--- assets/js/booking/booking.js
+++ assets/js/booking/booking.js
@@ -212,6 +212,8 @@ function checkoutPanel(data){
       tr({class: 'space-row'}, td(), td())
     ];
 
+    var disclaimW = '90%';
+
     return element(div({class: 'checkout-panel',},
       div({class: 'title'},
         h1({class: 'inline-block'}, "CHECKOUT"),
@@ -299,7 +301,7 @@ function checkoutPanel(data){
           tr({class: 'disclaimer-row'},
             td({colspan: 2},
               div({class: 'disclaimer-checkbox'}, input({type: 'checkbox', name: 'disclaim1'})),
-              div({class: 'disclaimer'},
+              div({class: 'disclaimer', style: 'width: '+disclaimW+';'},
 "I understand that if I or my guests arrive intoxicated, I will not be allowed entry and tickets will be forfeited."
               )
             )
@@ -307,7 +309,7 @@ function checkoutPanel(data){
           tr({class: 'disclaimer-row'},
             td({colspan: 2},
               div({class: 'disclaimer-checkbox'},input({type: 'checkbox', name: 'disclaim2'})),
-              div({class: 'disclaimer'},
+              div({class: 'disclaimer', style: 'width: '+disclaimW+';'},
 "I understand that if I bring children under the age of 12, all 8 tickets must be reserved by members of my group."
               )
             )
@@ -315,7 +317,7 @@ function checkoutPanel(data){
           tr({class: 'disclaimer-row'},
             td({colspan: 2},
               div({class: 'disclaimer-checkbox'}, input({type: 'checkbox', name: 'disclaim3'})),
-              div({class: 'disclaimer'},
+              div({class: 'disclaimer', style: 'width: '+disclaimW+';'},
 "I understand there are no refunds or cancellations after purchasing a ticket."
               )
             )
